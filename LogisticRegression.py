652c8f873779f3684fed5e6201d82e9ecb637825
Siyang <siyangqiu@stu.hit.edu.cn>
2022-08-24 08:49:02 +0800

    Finalized Logistic Regression script

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torch.utils.data import random_split, DataLoader
-from tqdm import tqdm, trange
+from tqdm.notebook import tqdm, trange
 
 '''
 Helper function to display an image
@@ -33,7 +33,7 @@ class MNIST_Logistic_Regression(nn.Module):
         self.linear = nn.Linear(28*28, 10)
         
     def forward(self, x):
-        xb = xb.reshape(-1, 784)
+        x = x.reshape(-1, 784)
         out = self.linear(x)
         return out
     
@@ -60,19 +60,14 @@ class MNIST_Logistic_Regression(nn.Module):
     def epoch_end(self, epoch, result):
         print("Epoch [{}], val_loss: {:.4f}, val_acc: {:.4f}".format(epoch, result['val_loss'], result['val_acc']))
 
-def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD, plot=False):
+def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD, plot=False, leaveProgressBar=False):
     optimizer = opt_func(model.parameters(), lr)
     history = [] # for recording epoch-wise results
-    if plot:
-        plt.figure()
-        plt.xlabel('epoch')
-        plt.ylabel('accuracy')
-        plt.title('Accuracy vs. No. of epochs')
     
-    for epoch in trange(epochs):
+    for epoch in trange(epochs, leave=leaveProgressBar):
         
         # Training Phase 
-        for batch in train_loader:
+        for batch in tqdm(train_loader, leave=leaveProgressBar):
             loss = model.training_step(batch)
             loss.backward()
             optimizer.step()
@@ -82,12 +77,6 @@ def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD, p
         result = evaluate(model, val_loader)
         model.epoch_end(epoch, result)
         history.append(result)
-
-        # plot current accuracy
-        if plot:
-            accuracies = [result['val_acc'] for result in history]
-            plt.plot(accuracies, '-x')
-            plt.show()
 
     return history
 
@@ -99,38 +88,3 @@ def evaluate(model, val_loader):
 def accuracy(outputs, labels):
     _, preds = torch.max(outputs, dim=1)
     return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-
-'''
-Prepare dataset
-'''
-batchSize = 100
-trainData = datasets.MNIST(root="./datasets", train=True, transform=transforms.ToTensor(), download=True)
-testData = datasets.MNIST(root="./datasets", train=False, transform=transforms.ToTensor(), download=True)
-trainSet, valSet = random_split(trainData, [50000, 10000])
-trainLoader = DataLoader(trainSet, batch_size=batchSize, shuffle=True)
-valLoader = DataLoader(valSet, batch_size=batchSize, shuffle=False)
-testLoader = DataLoader(testData, batch_size=batchSize, shuffle=False)
-
-'''
-Train model
-'''
-model = MNIST_Logistic_Regression()
-history1 = fit(5, 0.001, model, trainLoader, valLoader)
-
-'''
-Test model
-'''
-# Overall test accuracy
-result = evaluate(model, testLoader)
-print(f"Test accuracy={result['val_acc']}")
-
-def predict_image(img, model):
-    x = img.unsqueeze(0)
-    y = model(x)
-    _, preds = torch.max(y, dim=1)
-    return preds[0].item()
-
-keyboard = input("enter n to see next image, or anything else to exit: ")
-while keyboard == 'n':
-    img, label = testData[int(torch.randint(0, len(testData), (1,1)))]
-    displayImage(img, label, predict_image(img, model))
